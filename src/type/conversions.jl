b0db41c957b53902fec2c4efb464c420ea121559
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-03-30 01:18:07 -0400

    as

---
+++ src/type/conversions.jl
@@ -0,0 +1,16 @@
+"""
+    as(AbstractType, x)
+
+Provide `x` recast the size-matched concrete type <: AbstractType
+
+- as(Unsigned,    0x05) == 0x05
+- as(Unsigned, Int8(5)) == 0x05
+
+- as(Signed,    5) = 5
+- as(Signed, 0x05) = Int8(5)
+
+- as(AbstractFloat, 5.0) = 5.0
+- as(AbstractFloat, UInt16(5)) = Float16(5.0)
+- as(AbstractFloat, Int16(-5)) = Float16(-5.0)
+"""
+Base.@nospecializeinfer as(::Type{T}, @nospecialize(x::T)) where {T} = x
