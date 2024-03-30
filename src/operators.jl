de1316252e8691330572493861eb5726b26023d3
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-03-30 02:16:31 -0400

    mask hilo

--- src/operators.jl
+++ src/operators.jl
@@ -7,3 +7,11 @@ maskhi(n::T) where {T<:Integer} = masklo(n) << (8sizeof(T)-n)
 
 maskhi(::Type{T1}, n::T2) where {T1<:Integer, T2<:Integer} = 
     masklo(n) << (8sizeof(T)-n)
+
+filterlo(n::T) where {T<:Integer} = maskhi(8sizeof(T) - n)
+filterlo(::Type{T1}, n::T2) where {T1<:Integer, T2<:Integer} =
+    maskhi(8sizeof(T1) - n)
+
+filterhi(n::T) where {T<:Integer} = masklo(8sizeof(T) - n)
+filterhi(::Type{T1}, n::T2) where {T1<:Integer, T2<:Integer} =
+    masklo(8sizeof(T1) - n)
