6ad2bbc56b386150dd3a2b965cdcf23dd965b426
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-03-30 01:51:41 -0400

    export Float

--- src/type/conversions.jl
+++ src/type/conversions.jl
@@ -18,8 +18,8 @@ Base.@nospecializeinfer as(::Type{T}, @nospecialize(x::T)) where {T} = x
 Base.@nospecializeinfer as(::Type{Integer},  @nospecialize(x::Integer))  = x
 Base.@nospecializeinfer as(::Type{Unsigned}, @nospecialize(x::Unsigned)) = x
 Base.@nospecializeinfer as(::Type{Signed},   @nospecialize(x::Signed))   = x
-
 Base.@nospecializeinfer as(::Type{AbstractFloat}, @nospecialize(x::AbstractFloat)) = x
+Base.@nospecializeinfer as(::Type{Float}, @nospecialize(x::AbstractFloat)) = x
 
 as(::Type{Signed}, @nospecialize(x::Unsigned)) = signed(x)
 as(::Type{Unsigned}, @nospecialize(x::Signed)) = unsigned(x)
@@ -27,10 +27,12 @@ as(::Type{Unsigned}, @nospecialize(x::Signed)) = unsigned(x)
 for (F, U, I) in ((Float16, UInt16, Int16), (Float32, UInt32, Int32),
                   (Float64, UInt64, Int64))
     @eval begin
-        as(Type{AbstractFloat}, x::$U) = reinterpret($F, x)
-        as(Type{AbstractFloat}, x::$I) = reinterpret($F, x)
         as(Type{Unsigned}, x::$F) = reinterpret($U, x)
         as(Type{Signed}, x::$F) = reinterpret($I, x)
+        as(Type{AbstractFloat}, x::$U) = reinterpret($F, x)
+        as(Type{AbstractFloat}, x::$I) = reinterpret($F, x)
+        as(Type{Float}, x::$U) = reinterpret($F, x)
+        as(Type{Float}, x::$I) = reinterpret($F, x)
     end
 end
 
