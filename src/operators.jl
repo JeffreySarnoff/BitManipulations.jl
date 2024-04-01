8b07fe218d9644b7155d7a0ae6347734d4d36107
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-04-01 15:14:43 -0400

    bitreverse

--- src/operators.jl
+++ src/operators.jl
@@ -37,3 +37,54 @@ leading_one(x::T) where {T<:BitInteger} =
 trailing_one(x::T) where {T<:BitInteger} = 
     one(T) << ((bitsof(T) - one(Int16)) -  Base.cttz_int(x))
 
+function bitreverse(x::UInt8)
+    x = ((x >> 1) & 0x55) | ((x & 0x55) << 1)
+    x = ((x >> 2) & 0x33) | ((x & 0x33) << 2)
+    x = (x >> 4) | (x << 4)
+    x
+end
+
+function bitreverse(x::UInt16)
+    x = ((x >> 1) & 0x5555) | ((x & 0x5555) << 1)
+    x = ((x >> 2) & 0x3333) | ((x & 0x3333) << 2)
+    x = ((x >> 4) & 0x0F0F) | ((x & 0x0F0F) << 4)
+    x = (x >> 8) | (x << 8)
+    x
+end
+
+function bitreverse(x::UInt32)
+    x = ((x >> 1) & 0x55555555) | ((x & 0x55555555) << 1)
+    x = ((x >> 2) & 0x33333333) | ((x & 0x33333333) << 2)
+    x = ((x >> 4) & 0x0F0F0F0F) | ((x & 0x0F0F0F0F) << 4)
+    x = ((x >> 8) & 0x00FF00FF) | ((x & 0x00FF00FF) << 8)
+    x = (x >> 16)  | (x << 16)
+    x
+end
+
+function bitreverse(x::UInt64)
+    x = ((x >> 1) & 0x555555555555555) | ((x & 0x555555555555555) << 1)
+    x = ((x >> 2) & 0x3333333333333333) | ((x & 0x3333333333333333) << 2)
+    x = ((x >> 4) & 0x0F0F0F0F0F0F0F0F) | ((x & 0x0F0F0F0F0F0F0F0F) << 4)
+    x = ((x >> 8) & 0x00FF00FF00FF00FF) | ((x & 0x00FF00FF00FF00FF) << 8)
+    x = ((x >> 16) & 0x0000FFFF0000FFFF) | ((x & 0x0000FFFF0000FFFF) << 16)
+    x = (x >> 32)  | (x << 32)
+    x
+end
+
+function bitreverse(x::UInt128)
+    x = ((x >> 1) & 0x555555555555555555555555555555) | ((x & 0x555555555555555555555555555555) << 1)
+    x = ((x >> 2) & 0x33333333333333333333333333333333) | ((x & 0x33333333333333333333333333333333) << 2)
+    x = ((x >> 4) & 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) | ((x & 0x0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F) << 4)
+    x = ((x >> 8) & 0x00FF00FF00FF00FF00FF00FF00FF00FF) | ((x & 0x00FF00FF00FF00FF00FF00FF00FF00FF) << 8)
+    x = ((x >> 16) & 0x0000FFFF0000FFFF0000FFFF0000FFFF) | ((x & 0x0000FFFF0000FFFF0000FFFF0000FFFF) << 16)
+    x = ((x >> 32) & 0x00000000FFFFFFFF00000000FFFFFFFF) | ((x & 0x00000000FFFFFFFF00000000FFFFFFFF) << 32)
+    x = (x >> 64)  | (x << 64)
+    x
+end
+
+bitreverse(x::Int8)  = reinterpret(Int8, bitreverse(reinterpret(UInt8,x)))
+bitreverse(x::Int16) = reinterpret(Int16, bitreverse(reinterpret(UInt16,x)))
+bitreverse(x::Int32) = reinterpret(Int32, bitreverse(reinterpret(UInt32,x)))
+bitreverse(x::Int64) = reinterpret(Int64, bitreverse(reinterpret(UInt64,x)))
+bitreverse(x::Int128) = reinterpret(Int128, bitreverse(reinterpret(UInt128,x)))
+
